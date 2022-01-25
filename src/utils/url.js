1f8e152a7cf3ecc077aefa9aeaa641aec096c297
antenna3mt <antenna3mt@outlook.com>
2022-01-25 22:24:02 +0800

    util-url

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,72 +1,26 @@
-/**
- * Encoding Rules ␣ = Space Path: ␣ " < > # ? { } Query: ␣ " < > # & = Hash: ␣ "
- * < > `
- *
- * On top of that, the RFC3986 (https://tools.ietf.org/html/rfc3986#section-2.2)
- * defines some extra characters to be encoded. Most browsers do not encode them
- * in encodeURI https://github.com/whatwg/url/issues/369, so it may be safer to
- * also encode `!'()*`. Leaving unencoded only ASCII alphanumeric(`a-zA-Z0-9`)
- * plus `-._~`. This extra safety should be applied to query by patching the
- * string returned by encodeURIComponent encodeURI also encodes `[\]^`. `\`
- * should be encoded to avoid ambiguity. Browsers (IE, FF, C) transform a `\`
- * into a `/` if directly typed in. The _backtick_ (`````) should also be
- * encoded everywhere because some browsers like FF encode it when directly
- * written while others don't. Safari and IE don't encode ``"<>{}``` in hash.
- */
-// const EXTRA_RESERVED_RE = /[!'()*]/g
-// const encodeReservedReplacer = (c: string) => '%' + c.charCodeAt(0).toString(16)
+export const HASH_RE = /#/g // %23
+export const AMPERSAND_RE = /&/g // %26
+export const SLASH_RE = /\//g // %2F
+export const EQUAL_RE = /=/g // %3D
+export const IM_RE = /\?/g // %3F
+export const PLUS_RE = /\+/g // %2B
 
-const HASH_RE = /#/g // %23
-const AMPERSAND_RE = /&/g // %26
-const SLASH_RE = /\//g // %2F
-const EQUAL_RE = /=/g // %3D
-const IM_RE = /\?/g // %3F
-const PLUS_RE = /\+/g // %2B
-/**
- * NOTE: It's not clear to me if we should encode the + symbol in queries, it
- * seems to be less flexible than not doing so and I can't find out the legacy
- * systems requiring this for regular requests like text/html. In the standard,
- * the encoding of the plus character is only mentioned for
- * application/x-www-form-urlencoded
- * (https://url.spec.whatwg.org/#urlencoded-parsing) and most browsers seems lo
- * leave the plus character as is in queries. To be more flexible, we allow the
- * plus character on the query but it can also be manually encoded by the user.
- *
- * Resources:
- * - https://url.spec.whatwg.org/#urlencoded-parsing
- * - https://stackoverflow.com/questions/1634271/url-encoding-the-space-character-or-20
- */
+export const ENC_BRACKET_OPEN_RE = /%5B/g // [
+export const ENC_BRACKET_CLOSE_RE = /%5D/g // ]
+export const ENC_CARET_RE = /%5E/g // ^
+export const ENC_BACKTICK_RE = /%60/g // `
+export const ENC_CURLY_OPEN_RE = /%7B/g // {
+export const ENC_PIPE_RE = /%7C/g // |
+export const ENC_CURLY_CLOSE_RE = /%7D/g // }
+export const ENC_SPACE_RE = /%20/g // }
 
-const ENC_BRACKET_OPEN_RE = /%5B/g // [
-const ENC_BRACKET_CLOSE_RE = /%5D/g // ]
-const ENC_CARET_RE = /%5E/g // ^
-const ENC_BACKTICK_RE = /%60/g // `
-const ENC_CURLY_OPEN_RE = /%7B/g // {
-const ENC_PIPE_RE = /%7C/g // |
-const ENC_CURLY_CLOSE_RE = /%7D/g // }
-const ENC_SPACE_RE = /%20/g // }
-
-/**
- * Encode characters that need to be encoded on the path, search and hash
- * sections of the URL.
- *
- * @internal
- * @param text - string to encode
- * @returns encoded string
- */
-function commonEncode(text) {
-    return encodeURI('' + text)
+export function commonEncode(text) {
+    return encodeURIComponent('' + text)
         .replace(ENC_PIPE_RE, '|')
         .replace(ENC_BRACKET_OPEN_RE, '[')
         .replace(ENC_BRACKET_CLOSE_RE, ']')
 }
 
-/**
- * Encode characters that need to be encoded on the hash section of the URL.
- *
- * @param text - string to encode
- * @returns encoded string
- */
 export function encodeHash(text) {
     return commonEncode(text)
         .replace(ENC_CURLY_OPEN_RE, '{')
@@ -74,17 +28,9 @@ export function encodeHash(text) {
         .replace(ENC_CARET_RE, '^')
 }
 
-/**
- * Encode characters that need to be encoded query values on the query
- * section of the URL.
- *
- * @param text - string to encode
- * @returns encoded string
- */
 export function encodeQueryValue(text) {
     return (
         commonEncode(text)
-            // Encode the space as +, encode the + to differentiate it from the space
             .replace(PLUS_RE, '%2B')
             .replace(ENC_SPACE_RE, '+')
             .replace(HASH_RE, '%23')
@@ -96,100 +42,18 @@ export function encodeQueryValue(text) {
     )
 }
 
-/**
- * Like `encodeQueryValue` but also encodes the `=` character.
- *
- * @param text - string to encode
- */
 export function encodeQueryKey(text) {
     return encodeQueryValue(text).replace(EQUAL_RE, '%3D')
 }
 
-/**
- * Encode characters that need to be encoded on the path section of the URL.
- *
- * @param text - string to encode
- * @returns encoded string
- */
 export function encodePath(text) {
     return commonEncode(text).replace(HASH_RE, '%23').replace(IM_RE, '%3F')
 }
 
-/**
- * Encode characters that need to be encoded on the path section of the URL as a
- * param. This function encodes everything {@link encodePath} does plus the
- * slash (`/`) character. If `text` is `null` or `undefined`, returns an empty
- * string instead.
- *
- * @param text - string to encode
- * @returns encoded string
- */
 export function encodeParam(text) {
     return text == null ? '' : encodePath(text).replace(SLASH_RE, '%2F')
 }
 
-/**
- * Decode text using `decodeURIComponent`. Returns the original text if it
- * fails.
- *
- * @param text - string to decode
- * @returns decoded string
- */
-export function decode(text) {
-    try {
-        return decodeURIComponent('' + text)
-    } catch (err) {
-        return '' + text
-    }
-
-}
-
-/**
- * Transforms a queryString into a {@link LocationQuery} object. Accept both, a
- * version with the leading `?` and without Should work as URLSearchParams
- * @internal
- *
- * @param search - search string to parse
- * @returns a query object
- */
-export function parseQuery(search) {
-    const query = {}
-    // avoid creating an object with an empty key and empty value
-    // because of split('&')
-    if (search === '' || search === '?') return query
-    const hasLeadingIM = search[0] === '?'
-    const searchParams = (hasLeadingIM ? search.slice(1) : search).split('&')
-    for (let i = 0; i < searchParams.length; ++i) {
-        // pre decode the + into space
-        const searchParam = searchParams[i].replace(PLUS_RE, ' ')
-        // allow the = character
-        const eqPos = searchParam.indexOf('=')
-        const key = decode(eqPos < 0 ? searchParam : searchParam.slice(0, eqPos))
-        const value = eqPos < 0 ? null : decode(searchParam.slice(eqPos + 1))
-
-        if (key in query) {
-            // an extra variable for ts types
-            let currentValue = query[key]
-            if (!Array.isArray(currentValue)) {
-                currentValue = query[key] = [currentValue]
-            }
-            currentValue.push(value)
-        } else {
-            query[key] = value
-        }
-    }
-    return query
-}
-
-/**
- * Stringifies a {@link LocationQueryRaw} object. Like `URLSearchParams`, it
- * doesn't prepend a `?`
- *
- * @internal
- *
- * @param query - query object to stringify
- * @returns string version of the query without the leading `?`
- */
 export function stringifyQuery(query) {
     let search = ''
     for (let key in query) {
@@ -221,6 +85,35 @@ export function stringifyQuery(query) {
     return search
 }
 
+export function parseQuery(search) {
+    const query = {}
+    // avoid creating an object with an empty key and empty value
+    // because of split('&')
+    if (search === '' || search === '?') return query
+    const hasLeadingIM = search[0] === '?'
+    const searchParams = (hasLeadingIM ? search.slice(1) : search).split('&')
+    for (let i = 0; i < searchParams.length; ++i) {
+        // pre decode the + into space
+        const searchParam = searchParams[i].replace(PLUS_RE, ' ')
+        // allow the = character
+        const eqPos = searchParam.indexOf('=')
+        const key = decode(eqPos < 0 ? searchParam : searchParam.slice(0, eqPos))
+        const value = eqPos < 0 ? null : decode(searchParam.slice(eqPos + 1))
+
+        if (key in query) {
+            // an extra variable for ts types
+            let currentValue = query[key]
+            if (!Array.isArray(currentValue)) {
+                currentValue = query[key] = [currentValue]
+            }
+            currentValue.push(value)
+        } else {
+            query[key] = value
+        }
+    }
+    return query
+}
+
 export function normalizeQuery(query) {
     const normalizedQuery = {}
 
@@ -236,4 +129,193 @@ export function normalizeQuery(query) {
     }
 
     return normalizedQuery
+}
+
+export function decode(text) {
+    try {
+        return decodeURIComponent('' + text)
+    } catch (err) {
+        return '' + text
+    }
+
+}
+
+
+export function parseUrl(url) {
+    const ub = new UrlBuilder();
+
+    let balance = url;
+
+    // fragment
+    balance = (url => {
+        const arr = url.split('#');
+        ub.fragment(arr.length > 1 ? decode(arr[1]) : '');
+        return arr[0];
+    })(balance);
+
+    // query
+    balance = (url => {
+        const arr = url.split('?');
+        ub.query(arr.length > 1 ? parseQuery(arr[1]) : {});
+        return arr[0];
+    })(balance);
+
+    // protocol
+    balance = (url => {
+        const arr = url.split('://');
+        if (arr.length > 1) {
+            ub.protocol(arr[0]);
+            return arr[1];
+        } else
+            return arr[0];
+    })(balance);
+
+    // path
+    balance = (url => {
+        const arr = url.split('/');
+        ub.path(arr.length > 1 ? "/" + arr.slice(1).join('/') : '');
+        return arr[0];
+    })(balance);
+
+    // login
+    balance = (url => {
+        const arr = url.split('@');
+        if (arr.length > 1) {
+            const [username, password] = arr[0].split(':');
+            ub.username(username);
+            ub.password(password || '');
+            return arr[1];
+        } else {
+            return arr[0];
+        }
+    })(balance);
+
+    // port
+
+    balance = (url => {
+        const arr = url.split(':');
+        ub.port(arr.length > 1 ? arr[1] : '');
+        return arr[0];
+    })(balance);
+
+    ub.host(balance);
+
+    return ub;
+}
+
+// [protocol]://[username]:[password]@[host]:[port][path]?[query]#[fragment]
+export class UrlBuilder {
+    constructor(baseUrl) {
+        let url = {};
+        if (typeof baseUrl === 'string') {
+            url = parseUrl(baseUrl);
+        }
+
+        this._protocol = url._protocol || '';
+        this._username = url._username || '';
+        this._password = url._password || '';
+        this._host = url._host || '';
+        this._port = url._port || '';
+        this._path = url._path || '';
+        this._query = url._query || {};
+        this._fragment = url._fragment || '';
+
+        Object.apply()
+    }
+    protocol(value) {
+        if (value === undefined) return this._protocol
+
+        this._protocol = value || '';
+        return this;
+    }
+    username(value) {
+        if (value === undefined) return this._username
+
+        this._username = value || '';
+        return this;
+    }
+    password(value) {
+        if (value === undefined) return this._password
+
+        this._password = value || '';
+        return this;
+    }
+    host(value) {
+        if (value === undefined) return this._host
+
+        this._host = value || '';
+        return this;
+    }
+    port(value) {
+        if (value === undefined) return this._port
+
+        this._port = value || '';
+        return this;
+    }
+    path(value) {
+        if (value === undefined) return this._path
+
+        this._path = value || '';
+        return this;
+    }
+    query(obj) {
+        if (obj === undefined) return this._query
+
+        this._query = normalizeQuery(obj || {});
+        return this;
+    }
+    fragment(value) {
+        if (value === undefined) return this._fragment
+
+        this._fragment = value || '';
+        return this;
+    }
+
+    ToProtocol() {
+        return this._protocol ? this._protocol + "://" : "";
+    }
+
+    ToLogin() {
+        let url = "";
+        if (this._username) {
+            url += this._username
+            if (this._password) url += ":" + this._password;
+            url += '@';
+        }
+        return url;
+    }
+
+    ToHost() {
+        return this._host;
+    }
+
+    ToPort() {
+        return this._port ? ":" + this._port : "";
+    }
+
+    ToPath() {
+        return this._path;
+    }
+
+    ToQuery() {
+        const queryString = stringifyQuery(this._query);
+        console.log(queryString);
+        return queryString && queryString.length > 0 ? "?" + queryString : "";
+    }
+
+    ToFragment() {
+        return this._fragment ? "#" + encodeHash(this._fragment) : '';
+    }
+
+    ToOrigin() {
+        return this.ToProtocol() + this.ToHost() + this.ToPort();
+    }
+
+    toHref() {
+        return "" + this.ToProtocol() + this.ToLogin() + this.ToHost() + this.ToPort() + this.ToPath() + this.ToQuery();
+    }
+
+    toString() {
+        return this.toHref() + this.ToFragment();
+    }
 }
--- tests/unit/utils.url.spec.js
+++ tests/unit/utils.url.spec.js
@@ -1,13 +1,167 @@
-import { parseQuery, stringifyQuery } from '@/utils/url';
+import { UrlBuilder } from '@/utils/url';
 import { expect } from 'chai';
 
 describe('utils.url', () => {
-    describe('parseQuery', () => {
-        it('normal case works', () => {
-            const urlString = "a=a&b=b";
-            const query = parseQuery(urlString);
-            const urlString2 = stringifyQuery(query);
-            expect(urlString2).to.be.equal(urlString);
+    describe('UrlBuilder', () => {
+        it('[host] ', () => {
+            const url = 'www.my-size.com';
+            const ub = new UrlBuilder().host(url);
+            expect(ub.toString()).to.equal(url);
+
+            const ub2 = new UrlBuilder(url)
+            expect(ub2.host()).to.equal('www.my-size.com');
+            expect(ub2.toString()).to.equal(url);
+        })
+        it('[host]:[port]', () => {
+            const url = 'www.my-size.com:9000';
+            const ub = new UrlBuilder();
+            ub.host('www.my-size.com').port('9000');
+            expect(ub.toString()).to.equal(url);
+
+            const ub2 = new UrlBuilder(url)
+            expect(ub2.host()).to.equal('www.my-size.com');
+            expect(ub2.port()).to.equal('9000');
+            expect(ub2.toString()).to.equal(url);
+        })
+        it('[username]@[host]', () => {
+            const url = 'jiyi@www.my-size.com';
+
+            const ub = new UrlBuilder().host('www.my-size.com').username('jiyi');
+            expect(ub.toString()).to.equal(url);
+
+            const ub2 = new UrlBuilder(url)
+            expect(ub2.host()).to.equal('www.my-size.com');
+            expect(ub2.username()).to.equal('jiyi');
+            expect(ub2.toString()).to.equal(url);
+        })
+        it('[username]:[password]@[host]', () => {
+            const host = 'www.my-size.com';
+            const username = 'jiyi';
+            const password = 'xdq';
+            const url = username + ':' + password + '@' + host;
+
+            const ub = new UrlBuilder().host(host).username(username).password(password);
+            expect(ub.toString()).to.equal(url);
+
+            const ub2 = new UrlBuilder(url);
+            expect(ub2.host()).to.equal(host);
+            expect(ub2.username()).to.equal(username);
+            expect(ub2.password()).to.equal(password);
+            expect(ub2.toString()).to.equal(url);
+        })
+        it('[username]:[password]@[host]:[port]', () => {
+            const host = 'www.my-size.com';
+            const username = 'jiyi';
+            const password = 'xdq';
+            const port = '9000';
+            const url = username + ':' + password + '@' + host + ':' + port;
+
+            const ub = new UrlBuilder().host(host).username(username).password(password).port(port);
+            expect(ub.toString()).to.equal(url);
+
+            const ub2 = new UrlBuilder(url);
+            expect(ub2.host()).to.equal(host);
+            expect(ub2.username()).to.equal(username);
+            expect(ub2.password()).to.equal(password);
+            expect(ub2.port()).to.equal(port);
+            expect(ub2.toString()).to.equal(url);
+        })
+        it('[protocal]://[host]', () => {
+            const protocol = 'https';
+            const host = 'www.my-size.com';
+            const url = protocol + '://' + host;
+
+            const ub = new UrlBuilder().protocol(protocol).host(host);
+            expect(ub.toString()).to.equal(url);
+
+            const ub2 = new UrlBuilder(url);
+            expect(ub2.host()).to.equal(host);
+            expect(ub2.protocol()).to.equal(protocol);
+            expect(ub2.toString()).to.equal(url);
+        })
+        it('[protocal]://[host]#[fragment]', () => {
+            const protocol = 'https';
+            const host = 'www.my-size.com';
+            const fragment = 'state';
+            const url = protocol + '://' + host + '#' + fragment;
+
+            const ub = new UrlBuilder().protocol(protocol).host(host).fragment(fragment);
+            expect(ub.toString()).to.equal(url);
+
+            const ub2 = new UrlBuilder(url);
+            expect(ub2.host()).to.equal(host);
+            expect(ub2.protocol()).to.equal(protocol);
+            expect(ub2.fragment()).to.equal(fragment);
+            expect(ub2.toString()).to.equal(url);
+        })
+
+        it('[protocal]://[host][path]#[fragment]', () => {
+            const protocol = 'https';
+            const host = 'www.my-size.com';
+            const path = '/path/';
+            const fragment = 'state';
+            const url = protocol + '://' + host + path + '#' + fragment;
+
+            const ub = new UrlBuilder().protocol(protocol).host(host).path(path).fragment(fragment);
+            expect(ub.toString()).to.equal(url);
+
+            const ub2 = new UrlBuilder(url);
+            expect(ub2.protocol()).to.equal(protocol);
+            expect(ub2.host()).to.equal(host);
+            expect(ub2.path()).to.equal(path);
+            expect(ub2.fragment()).to.equal(fragment);
+            expect(ub2.toString()).to.equal(url);
+        })
+
+        it('[protocal]://[username]:[password]@[host]:[port][path]#[fragment]', () => {
+            const protocol = 'https';
+            const username = 'jiyi';
+            const password = 'xdq';
+            const host = 'www.my-size.com';
+            const port = '9000';
+            const path = '/path/';
+            const fragment = 'state';
+            const url = protocol + '://' + username + ':' + password + '@' + host + ":" + port + path + '#' + fragment;
+
+            const ub = new UrlBuilder()
+                .protocol(protocol).username(username).password(password)
+                .host(host).port(port).path(path).fragment(fragment)
+            expect(ub.toString()).to.equal(url);
+
+            const ub2 = new UrlBuilder(url);
+            expect(ub2.protocol()).to.equal(protocol);
+            expect(ub2.username()).to.equal(username);
+            expect(ub2.password()).to.equal(password);
+            expect(ub2.host()).to.equal(host);
+            expect(ub2.port()).to.equal(port);
+            expect(ub2.path()).to.equal(path);
+            expect(ub2.fragment()).to.equal(fragment);
+            expect(ub2.toString()).to.equal(url);
+        })
+
+        it('[protocol]://[host][path]?[query] TEST#1', () => {
+            const baseUrl = "https://www.my-site.com";
+            const url = "https://www.my-site.com?a=1&b=1";
+
+            const ub = new UrlBuilder(baseUrl).query({ a: "1", b: "1" });
+            expect(ub.toString()).to.equal(url);
+
+            const ub2 = new UrlBuilder(url);
+            expect(ub2.query()).to.deep.equal({ a: "1", b: "1" });
+            expect(ub2.toString()).to.equal(url);
+
+        })
+
+        it('[protocol]://[host][path]?[query] TEST#2', () => {
+            const baseUrl = "https://www.my-site.com";
+            const url = "https://www.my-site.com?url1=https%3A%2F%2Fwww.my-site.com+&url2=https%3A%2F%2Fwww.your-site.com";
+
+            const ub = new UrlBuilder(baseUrl).query({ url1: "https://www.my-site.com ", url2: "https://www.your-site.com" });
+            expect(ub.toString()).to.equal(url);
+
+            const ub2 = new UrlBuilder(url);
+            expect(ub2.query()).to.deep.equal({ url1: "https://www.my-site.com ", url2: "https://www.your-site.com" });
+            expect(ub2.toString()).to.equal(url);
         })
     })
 });
